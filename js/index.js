e41afcb89807cac4623fde1b82440a268be76145
jeonghyeonmin1 <hyunmin9808@naver.com>
2023-12-16 19:03:25 +0900

    알림 기능 개선

--- js/index.js
+++ js/index.js
@@ -24,6 +24,64 @@ const state = document.querySelector('#state')
 
 let flag1 = true
 
+askNotificationPermission();
+
+function makeNoti() {   
+  // 사용자 응답에 따라 단추를 보이거나 숨기도록 설정
+  if (Notification.permission === "denied" || Notification.permission === "default") {
+    alert("알림이 차단된 상태입니다. 알림 권한을 허용해주세요.");
+  } else {
+
+    let notification = new Notification("test", { // "test" => 제목
+      body: "눈을 깜빡이세요.", // 메세지
+      icon: `../img/eye.png`,
+    });
+
+
+    //알림 클릭 시 이벤트
+    // notification.addEventListener("click", () => {
+    //  window.open('https://www.naver.com/');
+    // });
+
+  }
+}
+
+function askNotificationPermission() {
+  console.log("권한 묻기");
+  // 권한을 실제로 요구하는 함수
+  function handlePermission(permission) {
+    // 사용자의 응답에 관계 없이 크롬이 정보를 저장할 수 있도록 함
+    if (!("permission" in Notification)) {
+      Notification.permission = permission;
+    }
+  }
+
+  // 브라우저가 알림을 지원하는지 확인
+  if (!("Notification" in window)) {
+    console.log("이 브라우저는 알림을 지원하지 않습니다.");
+  } else {
+    if (checkNotificationPromise()) {
+      Notification.requestPermission().then((permission) => {
+        handlePermission(permission);
+      });
+    } else {
+      Notification.requestPermission(function (permission) {
+        handlePermission(permission);
+      });
+    }
+  }
+}
+
+function checkNotificationPromise() {
+  try {
+    Notification.requestPermission().then();
+  } catch (e) {
+    return false;
+  }
+
+  return true;
+}
+
 middleDiv.forEach((div) => {
   div.addEventListener('click', () => { 
     middleDiv.forEach((div) => {
@@ -36,10 +94,12 @@ middleDiv.forEach((div) => {
       numCount = 0
       setInterval(() => {
         if(Math.floor(numCount/14) <= 10){
-            alert("눈을 깜빡이세요")
+          console.log("알림")
+          makeNoti()
+            
         }
         numCount = 0
-    }, 1000 * 60 * 30);   //시간 설정하는 부분
+    }, 1000 * 20);   //시간 설정하는 부분
     
     }else{
       state.innerHTML = "off"
